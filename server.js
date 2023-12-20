976ea7a72f9312c8b73de1d2d42af6a846812476
Taz Campbell <shelbyacampbell2724@gmail.com>
2023-12-19 23:25:07 -0500

    ul created, drinks capitalized and rendered

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const drinks = require('./models/drinks.js')
 
+
 const app = express()
 
 app.get("/", (req, res) => {
@@ -10,6 +11,14 @@ app.get("/drinks", (req, res) => {
     // res.send(drinks)
     res.render("index.ejs", {drinks})
 })
+
+for (const drink in drinks) {
+    if (typeof drinks[drink].name === "string") {
+      drinks[drink].name = drinks[drink].name.charAt(0).toUpperCase() + drinks[drink].name.slice(1);
+    }
+  }
+  
+//   console.log(drinks)
 
 app.listen(3000, () => {
     console.log('here for a drink')
