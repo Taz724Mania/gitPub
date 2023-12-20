ecf3abf205616bce4d878430b9713a2776ffcf70
Taz Campbell <shelbyacampbell2724@gmail.com>
2023-12-19 22:52:54 -0500

    drinks exported

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express')
-const app = express()
+const drinks = require('./models/drinks.js')
 
+const app = express()
 
 app.get("/", (req, res) => {
     res.send('Welcome to the Gitpub App!')
 })
-
+app.get("/drinks", (req, res) => {
+    res.send(drinks)
+})
 
 app.listen(3000, () => {
     console.log('here for a drink')
