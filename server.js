9328d38d38c6a5eb6be49f4f1905f836e73de9e5
Taz Campbell <shelbyacampbell2724@gmail.com>
2023-12-19 22:44:38 -0500

    get route added

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
 const express = require('express')
-const 
+const app = express()
+
+
+app.get("/", (req, res) => {
+    res.send('Welcome to the Gitpub App!')
+})
+
+
+app.listen(3000, () => {
+    console.log('here for a drink')
+})
